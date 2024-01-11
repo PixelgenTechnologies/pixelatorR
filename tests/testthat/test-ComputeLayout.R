21d77e6917218c20e2915b3dd39ff18b16545f52
ludvigla <ludvig.larsson@pixelgen.com>
2024-01-11 16:08:18 +0100

    Added new tests for ComputeLayout when passing a custom layout function

--- tests/testthat/test-ComputeLayout.R
+++ tests/testthat/test-ComputeLayout.R
@@ -38,6 +38,59 @@ test_that("ComputeLayout works as expected", {
   expect_equal(unique(radii) %>% length(), 1)
 })
 
+test_that("ComputeLayout works as expected with a custom layout function", {
+
+  custom_layout_fkn <- graphlayouts::layout_with_pmds
+
+  cg <- CellGraphs(se)[[colnames(se)[1]]]
+
+  # tbl_graph
+  expect_no_error(layout <- ComputeLayout(cg@cellgraph, custom_layout_function = custom_layout_fkn, custom_layout_function_args = list(pivots = 100)))
+  expect_equal(dim(layout), c(3525, 2))
+
+  # CellGraph
+  expect_no_error(cg_layout <- ComputeLayout(cg, custom_layout_function = custom_layout_fkn, custom_layout_function_args = list(pivots = 100)))
+  expect_equal(names(cg_layout@layout), "custom")
+  expect_equal(dim(cg_layout@layout[["custom"]]), c(3525, 2))
+
+  # CellGraphAssay
+  expect_no_error(cg_assay_layout <- ComputeLayout(se[["mpxCells"]], custom_layout_function = custom_layout_fkn, custom_layout_function_args = list(pivots = 100)))
+  expect_equal(names(CellGraphs(cg_assay_layout)[[1]]@layout), "custom")
+  expect_equal(dim(CellGraphs(cg_assay_layout)[[1]]@layout[["custom"]]), c(3525, 2))
+
+  # Seurat
+  expect_no_error(se_layout <- ComputeLayout(se, custom_layout_function = custom_layout_fkn, custom_layout_function_args = list(pivots = 100)))
+  expect_equal(names(CellGraphs(se_layout)[[1]]@layout), "custom")
+  expect_equal(dim(CellGraphs(se_layout)[[1]]@layout[["custom"]]), c(3525, 2))
+
+  # Test with new layout name
+  expect_no_error(se_layout <- ComputeLayout(se, custom_layout_function = custom_layout_fkn,
+                                             custom_layout_function_args = list(pivots = 100), custom_layout_name = "my_layout"))
+  expect_equal(names(CellGraphs(se_layout)[[1]]@layout), "my_layout")
+  expect_equal(dim(CellGraphs(se_layout)[[1]]@layout[["my_layout"]]), c(3525, 2))
+
+})
+
+test_that("ComputeLayout fails with an invalid input", {
+
+  custom_layout_fkn <- function(g) return("Invalid")
+
+  cg <- CellGraphs(se)[[colnames(se)[1]]]
+
+  # tbl_graph
+  expect_error(layout <- ComputeLayout(cg@cellgraph, custom_layout_function = custom_layout_fkn))
+
+  # CellGraph
+  expect_error(cg_layout <- ComputeLayout(cg, custom_layout_function = custom_layout_fkn))
+
+  # CellGraphAssay
+  expect_error(cg_assay_layout <- ComputeLayout(se[["mpxCells"]], custom_layout_function = custom_layout_fkn))
+
+  # Seurat
+  expect_error(se_layout <- ComputeLayout(se, custom_layout_function = custom_layout_fkn))
+
+})
+
 test_that("ComputeLayout fails when invalid input is provided", {
   expect_error(ComputeLayout("Invalid"))
   e <- tryCatch(se[["mpxCells"]] %>% ComputeLayout(layout_method = "Invalid", verbose = FALSE), error = function(e) e, silent = TRUE)
