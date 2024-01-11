743f35843c55d8c0c47c25a108802e121befaef6
ludvigla <ludvig.larsson@pixelgen.com>
2024-01-11 16:06:34 +0100

    Added option to pass a custom layout function to ComputeLayout

--- R/graph_layout.R
+++ R/graph_layout.R
@@ -10,13 +10,25 @@ globalVariables(
 #' Calculates a graph layout for a component's edgelist, and outputs a list
 #' with the bipartite graph, layout, and antibody counts per A pixel.
 #'
-#' @param layout_method The method for calculating the graph layout; PMDS, Fruchterman-Reingold (fr), Kamada-Kawai (kk), drl.
+#' @param layout_method The method for calculating the graph layout; PMDS,
+#' Fruchterman-Reingold (fr), Kamada-Kawai (kk), drl.
 #' @param dim An integer specifying the dimensions of the layout (2 or 3)
-#' @param normalize_layout Logical specifying whether the coordinate system for the layout should be scaled from -1 to 1.
-#' @param k The size of the neighborhood from which to pool counts from in the UPIA antibody count table. 0 is recommended.
-#' @param pivots Only used for "pmds" graph layout. See \code{?layout_with_pmds} for details
-#' @param project_on_unit_sphere Should the resulting layout be projected onto a unit sphere?
+#' @param normalize_layout Logical specifying whether the coordinate system
+#' for the layout should be scaled from -1 to 1.
+#' @param k The size of the neighborhood from which to pool counts from in
+#' the UPIA antibody count table. 0 is recommended.
+#' @param pivots Only used for "pmds" graph layout. See \code{?layout_with_pmds}
+#' for details
+#' @param project_on_unit_sphere Should the resulting layout be projected onto
+#'  a unit sphere?
 #' @param seed Set seed for reproducibility
+#' @param custom_layout_function A custom function for layout computation. The
+#' function should take a \code{tbl_graph} object and a \code{dim} value as
+#' input and return a matrix of dimensions NxD, where N is the number of nodes
+#' and D is equal to \code{dim}. Note that this will override the \code{layout_method}.
+#' @param custom_layout_function_args A list of arguments passed to \code{custom_layout_function}.
+#' The \code{dim} is automatically passed to \code{custom_layout_function} and should not be
+#' included in \code{custom_layout_function_args}.
 #'
 #' @import dplyr
 #' @importFrom igraph layout_with_fr layout_with_kk layout_with_drl
@@ -60,45 +72,70 @@ ComputeLayout.tbl_graph <- function (
   k = 0,
   pivots = 100,
   seed = 123,
+  custom_layout_function = NULL,
+  custom_layout_function_args = NULL,
   ...
 ) {
 
   # Validate input parameters
-  stopifnot("dim should be an integer vector of length 1" = inherits(dim, what = c("numeric", "integer")) & (length(dim) == 1),
-            "normalize_layout should be TRUE/FALSE" = is.logical(normalize_layout),
-            "k should set to 1 or 2" = inherits(k, what = c("numeric", "integer")) & (length(dim) == 1) & (dim %in% c(2, 3)),
-            "'seed' should be a numeric value" = inherits(seed, what = "numeric"))
-
-  # Check and select a layout method
-  layout_method <- match.arg(layout_method, choices = c("pmds", "fr", "kk", "drl"))
-
-  layout_function <- switch(
-    layout_method,
-    "fr" = layout_with_fr,
-    "kk" = layout_with_kk,
-    "drl" = layout_with_drl,
-    "pmds" = {
-      # Abort if pmds is selected and graphlayouts isn't installed
-      expect_graphlayouts()
-      graphlayouts::layout_with_pmds
-    })
+  stopifnot(
+    "dim should be an integer vector of length 1" =
+      inherits(dim, what = c("numeric", "integer")) &&
+      (length(dim) == 1),
+    "normalize_layout should be TRUE/FALSE" =
+      is.logical(normalize_layout),
+    "k should set to 1 or 2" =
+      inherits(k, what = c("numeric", "integer")) &&
+      (length(dim) == 1) & (dim %in% c(2, 3)),
+    "'seed' should be a numeric value" =
+      inherits(seed, what = "numeric")
+  )
 
   # Set seed
   old_seed <- .Random.seed
   set.seed(seed)
 
-  layout <-
-    object %>%
-    {
-      if (layout_method == "pmds") {
-        layout_function(., dim = dim, pivots = pivots)
-      } else {
-        layout_function(., dim = dim, ...)
-      }
-    } %>%
-    as_tibble(.name_repair = function(x) c("x", "y", "z")[1:length(x)]) %>%
-    bind_cols(as_tibble(object)) %>%
-    select(any_of(c("x", "y", "z")))
+  # validate and use custom layout function if available
+  if (!is.null(custom_layout_function)) {
+    .validate_custom_layout_function(custom_layout_function, custom_layout_function_args, dim)
+    layout_function <- custom_layout_function
+    layout_method <- "custom"
+
+    # Try custom layout function on tbl_graph
+    layout <- try({do.call(custom_layout_function, c(list(g = object), custom_layout_function_args))})
+    if (inherits(layout, what = "try-error"))
+      abort("'custom_layout_function' failed to compute layout")
+
+    .validate_custom_layout_function_results(layout, dim, n_nodes = length(object))
+
+  } else {
+    # Check and select a layout method
+    layout_method <- match.arg(layout_method, choices = c("pmds", "fr", "kk", "drl"))
+
+    layout_function <- switch(
+      layout_method,
+      "fr" = layout_with_fr,
+      "kk" = layout_with_kk,
+      "drl" = layout_with_drl,
+      "pmds" = {
+        # Abort if pmds is selected and graphlayouts isn't installed
+        expect_graphlayouts()
+        graphlayouts::layout_with_pmds
+      })
+
+    layout <-
+      object %>%
+      {
+        if (layout_method == "pmds") {
+          layout_function(., dim = dim, pivots = pivots)
+        } else {
+          layout_function(., dim = dim, ...)
+        }
+      } %>%
+      as_tibble(.name_repair = function(x) c("x", "y", "z")[1:length(x)]) %>%
+      bind_cols(as_tibble(object)) %>%
+      select(any_of(c("x", "y", "z")))
+  }
 
   # Project to unit sphere
   if (project_on_unit_sphere || normalize_layout) {
@@ -127,7 +164,10 @@ ComputeLayout.tbl_graph <- function (
   return(layout)
 }
 
-
+#' @param custom_layout_name A name for the layout computed with
+#' \code{custom_layout_function}. Should not be one of "pmds", "fr",
+#' "kk" or "drl".
+#'
 #' @rdname ComputeLayout
 #' @method ComputeLayout CellGraph
 #'
@@ -149,8 +189,19 @@ ComputeLayout.CellGraph <- function (
   k = 0,
   pivots = 100,
   seed = 123,
+  custom_layout_function = NULL,
+  custom_layout_function_args = NULL,
+  custom_layout_name = "custom",
   ...
 ) {
+
+  # Validate custom_layout_name
+  stopifnot(
+    "'custom_layout_name' should be a character of length 1" =
+      is.character(custom_layout_name) &&
+      (length(custom_layout_name) == 1)
+  )
+
   layout <-
     ComputeLayout(
       slot(object, name = "cellgraph"),
@@ -160,12 +211,22 @@ ComputeLayout.CellGraph <- function (
       project_on_unit_sphere = project_on_unit_sphere,
       k = k,
       pivots = pivots,
-      seed = seed
+      seed = seed,
+      custom_layout_function = custom_layout_function,
+      custom_layout_function_args = custom_layout_function_args,
+      ...
     )
   if (is.null(slot(object, name = "layout"))) {
     slot(object, name = "layout") <- list()
   }
-  slot(object, name = "layout")[[match.arg(layout_method, choices = c("pmds", "fr", "kk", "drl"))]] <- layout
+
+  # Add layout to CellGraph layout slot
+  if (!is.null(custom_layout_function)) {
+    slot(object, name = "layout")[[custom_layout_name]] <- layout
+  } else {
+    slot(object, name = "layout")[[match.arg(layout_method, choices = c("pmds", "fr", "kk", "drl"))]] <- layout
+  }
+
   return(object)
 }
 
@@ -193,6 +254,9 @@ ComputeLayout.CellGraphAssay <- function (
   pivots = 100,
   seed = 123,
   verbose = TRUE,
+  custom_layout_function = NULL,
+  custom_layout_function_args = NULL,
+  custom_layout_name = "custom",
   ...
 ) {
 
@@ -223,7 +287,11 @@ ComputeLayout.CellGraphAssay <- function (
       project_on_unit_sphere = project_on_unit_sphere,
       k = k,
       pivots = pivots,
-      seed = seed
+      seed = seed,
+      custom_layout_function = custom_layout_function,
+      custom_layout_function_args = custom_layout_function_args,
+      custom_layout_name = custom_layout_name,
+      ...
     )
     p()
     return(g)
@@ -256,6 +324,9 @@ ComputeLayout.Seurat <- function (
   pivots = 100,
   seed = 123,
   verbose = TRUE,
+  custom_layout_function = NULL,
+  custom_layout_function_args = NULL,
+  custom_layout_name = "custom",
   ...
 ) {
 
@@ -288,6 +359,9 @@ ComputeLayout.Seurat <- function (
       pivots = pivots,
       seed = seed,
       verbose = verbose,
+      custom_layout_function = custom_layout_function,
+      custom_layout_function_args = custom_layout_function_args,
+      custom_layout_name = custom_layout_name,
       ...
     )
 
@@ -295,3 +369,51 @@ ComputeLayout.Seurat <- function (
   return(object)
 }
 
+
+#' Validate a custom layout function
+#'
+#' @import rlang
+#' @import glue
+#'
+#' @noRd
+#'
+.validate_custom_layout_function <- function (
+  custom_layout_function,
+  custom_layout_function_args,
+  dim
+) {
+  # Make sure that a function is provided
+  if (!inherits(custom_layout_function, what = "function"))
+    abort(glue("Invalid class '{class(custom_layout_function)[1]}' for ",
+               "'custom_layout_function'. Expected a 'function'."))
+
+  # Make sure that the custom_layout_function_args is a list
+  if (!is.null(custom_layout_function_args)) {
+    if (!inherits(custom_layout_function_args, what = "list"))
+      abort(glue("'custom_layout_function_args' should be a list with function arguments"))
+  }
+}
+
+#' Validate the results from a custom_layout_function
+#'
+#' @noRd
+#'
+.validate_custom_layout_function_results <- function (
+  layout,
+  dim,
+  n_nodes
+) {
+
+  # Validate results
+  if (!inherits(layout, what = "matrix")) {
+    abort(glue("Expected a 'matrix' from 'custom_layout_function', but got a '{class(layout)[1]}'"))
+  }
+  if (ncol(layout) != dim) {
+    abort(glue("Expected dim={dim} from 'custom_layout_function' result, but got dim={ncol(layout)}"))
+  }
+  if (nrow(layout) != n_nodes) {
+    abort(glue("Invalid number of rows returned by 'custom_layout_function'. ",
+               "The number of rows in the layout should match the number of ",
+               "nodes in the graph"))
+  }
+}
