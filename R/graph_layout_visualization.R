a5cf4632a72fb4a3f22a2b5cda13d7fa83172c08
ludvigla <ludvig.larsson@pixelgen.com>
2024-01-16 09:06:20 +0100

    Fixed bug in Plot2DGraph with invalid ordering of layout coordinates. Added option to use log-scale transformation.

--- R/graph_layout_visualization.R
+++ R/graph_layout_visualization.R
@@ -13,8 +13,13 @@
 #' \code{\link{ComputeLayout}}
 #' @param colors A character vector of colors to color marker counts by
 #' @param map_nodes,map_edges Should nodes and/or edges be mapped?
+#' @param log_scale Convert node counts to log-scale with \code{logp}
 #' @param node_size Size of nodes
-#' @param edge_width Not yet implemented TODO
+#' @param edge_width Set the width of the edges if \code{map_edges = TRUE}
+#' @param show_Bnodes Should B nodes be included in the visualization?
+#' This option is only applicable to bipartite graphs. Note that by removing
+#' the B nodes, all edges are removed from the graph and hence, \code{map_edges}
+#' will have no effect.
 #' @param ... Not yet implemented
 #'
 #' @rdname Plot2DGraph
@@ -52,8 +57,10 @@ Plot2DGraph <- function (
   colors = c("lightgrey", "mistyrose", "red", "darkred"),
   map_nodes = TRUE,
   map_edges = FALSE,
+  log_scale = TRUE,
   node_size = 0.5,
   edge_width = 0.3,
+  show_Bnodes = FALSE,
   ...
 ) {
 
@@ -120,6 +127,22 @@ Plot2DGraph <- function (
 
     # unpack values
     graph <- component_graph@cellgraph
+
+    # Validate marker
+    if (!is.null(marker)) {
+      if (marker == "node_type") {
+        stopifnot(
+          "marker = 'node_type' can only be used for bipartite graphs" =
+            attr(graph, "type") == "bipartite"
+        )
+      } else {
+        if (!marker %in% colnames(component_graph@counts)) {
+          abort(glue("'{marker}' is missing from node count matrix ",
+                     "for component {cell_id}"))
+        }
+      }
+    }
+
     layout <- component_graph@layout[[layout_method]]
     if (length(graph) == 0)
       abort(glue("Missing cellgraph for component '{cell_id}'"))
@@ -131,7 +154,33 @@ Plot2DGraph <- function (
       if (marker != "node_type") {
           graph <- graph %N>%
             mutate(marker = component_graph@counts[, marker]) %>%
-            arrange(marker)
+            {
+              if (log_scale) {
+                mutate(., marker = log1p(marker))
+              } else {
+                .
+              }
+            }
+      }
+    }
+
+    # Remove B nodes if show_Bnodes=FALSE
+    if ((attr(graph, "type") == "bipartite") && !show_Bnodes) {
+      inds_keep <- (graph %>% pull(node_type)) == "A"
+      graph <- graph %>%
+        filter(node_type == "A")
+      layout <- layout[inds_keep, ]
+    }
+
+    # Rearrange by marker
+    if (!is.null(marker)) {
+      if (marker != "node_type") {
+        # Rearrange layout
+        order <- order(graph %>% pull(marker))
+        layout <- data.frame(layout, row.names = graph %>% pull(name))
+        graph <- graph %>%
+          arrange(marker)
+        layout <- layout[order, ] %>% as_tibble()
       }
     }
 
@@ -147,17 +196,37 @@ Plot2DGraph <- function (
       {
         # Add edges if map_edges=TRUE
         if (map_edges) {
-          geom_edge_link()
+          geom_edge_link(edge_width = edge_width)
         }
       } +
       {
         # Add nodes if map_nodes=TRUE
         if (map_nodes) {
-          geom_node_point(mapping = aes(color = marker), size = node_size)
+          if (!is.null(marker)) {
+            if (marker == "node_type") {
+              geom_node_point(mapping = aes(color = node_type), size = node_size)
+            } else {
+              geom_node_point(mapping = aes(color = marker), size = node_size)
+            }
+          } else {
+            geom_node_point(size = node_size)
+          }
         }
       } +
       coord_fixed() +
-      labs(title = glue("{nm}"), color = marker) +
+      {
+        if (!is.null(marker)) {
+          if (marker != "node_type") {
+            if (log_scale) {
+              labs(title = glue("{nm}"), color = paste0(marker, "\n(log-scaled)"))
+            } else {
+              labs(title = glue("{nm}"), color = marker)
+            }
+          } else {
+            labs(title = glue("{nm}"))
+          }
+        }
+      } +
       theme_void() +
       theme(plot.title = element_text(size = 10))
   })
