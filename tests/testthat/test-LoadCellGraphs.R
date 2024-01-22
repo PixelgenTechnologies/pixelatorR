613d2d1db04b2c94899a4747119861f9a6a518c0
ludvigla <ludvig.larsson@pixelgen.com>
2024-01-22 08:07:59 +0100

    Updated tests

--- tests/testthat/test-LoadCellGraphs.R
+++ tests/testthat/test-LoadCellGraphs.R
@@ -3,11 +3,17 @@ pxl_file <- system.file("extdata/PBMC_10_cells",
                         "Sample01_test.pxl",
                         package = "pixelatorR")
 seur_obj <- ReadMPX_Seurat(pxl_file, overwrite = TRUE, return_cellgraphassay = TRUE)
+seur_obj_merged <- merge(seur_obj, seur_obj, add.cell.ids = c("Sample1", "Sample2"))
 cg_assay <- seur_obj[["mpxCells"]]
+cg_assay_merged <- merge(cg_assay, cg_assay)
 
 test_that("LoadCellGraphs works for Seurat objects", {
+  # Single data set
   expect_no_error({seur_obj <- LoadCellGraphs(seur_obj, cells = colnames(seur_obj)[1])})
   expect_s4_class(seur_obj, "Seurat")
+
+  # Merged data set
+  seur_obj_merged <- LoadCellGraphs(seur_obj_merged, cells = colnames(seur_obj_merged)[1])
 })
 
 test_that("LoadCellGraphs works for CellGraphAssay objects", {
@@ -31,3 +37,12 @@ test_that("LoadCellGraphs works for CellGraphAssay objects", {
   expect_equal(attr(cg_assay@cellgraphs$RCVCMP0000000@cellgraph, "type"), "linegraph")
 })
 
+test_that("LoadCellGraphs works for FileSystemDataset", {
+  expect_no_error(el <- ReadMPX_arrow_edgelist(pxl_file))
+
+  # Single data set
+  expect_no_error({g_list <- LoadCellGraphs(el, cells = colnames(seur_obj)[1:2])})
+  expect_type(g_list, "list")
+  expect_s4_class(g_list[[1]], "CellGraph")
+  expect_equal(length(g_list), 2)
+})
