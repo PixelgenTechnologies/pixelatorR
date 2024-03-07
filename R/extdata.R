178a3488c4c2172641b7512b6fe5e1fe0b4fe28a
ludvigla <ludvig.larsson@pixelgen.com>
2024-03-07 17:12:21 +0100

    Updated dataset documentation

--- R/extdata.R
+++ R/extdata.R
@@ -1,10 +1,15 @@
 #' Mock Test data
 #'
-#' MPX data set of 5 immune cells
+#' MPX data set of 5 immune cells (link to [data resource](https://software.pixelgen.com/datasets))
 #' \itemize{
-#'    \item{[CD3 capping stimulated CD4 T cell: RCVCMP0000217](https://software.pixelgen.com/datasets/cd3-capping-v1.0-immunology-I)}
-#'    \item{[B cell: RCVCMP0000118, CD4 T cell: RCVCMP0000487, CD8 T cell: RCVCMP0000655](https://software.pixelgen.com/datasets/1k-human-pbmcs-v1.0-immunology-I))}
-#'    \item{[Migratory CD8 T cell with uropod: RCVCMP0000263](https://software.pixelgen.com/datasets/uropod-t-cells-v1.0-immunology-I)}
+#'    \item{1x CD3 capping stimulated CD4 T cell (RCVCMP0000217) \cr\cr
+#'    \strong{Data set:} CD3 Stimulated Human PBMCs SCSP v1.0 Kit, Immunology Panel I}
+#'    \item{1x B cell (RCVCMP0000118)\cr
+#'    1x CD4 T cell (RCVCMP0000487)\cr
+#'    1x CD8 T cell (RCVCMP0000655) \cr\cr
+#'    \strong{Data set:} 1k Human PBMCs, SCSP v1.0 Kit, Immunology Panel I}
+#'    \item{1x Migratory CD8 T cell with uropod (RCVCMP0000263) \cr\cr
+#'    \strong{Data set:} Uropod formation on T cells, SCSP v1.0 Kit, Immunology Panel I}
 #' }
 #'
 #' @name pxl_dataset
