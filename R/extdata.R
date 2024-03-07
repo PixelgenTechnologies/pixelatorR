42f0b56283a92b59ca78f3f1c5184e92a3fd622b
ludvigla <ludvig.larsson@pixelgen.com>
2024-03-07 16:45:52 +0100

    Addressed cell name comments.

--- R/extdata.R
+++ R/extdata.R
@@ -1,10 +1,10 @@
 #' Mock Test data
 #'
-#' MPX mock data set with 5 cells
+#' MPX data set of 5 immune cells
 #' \itemize{
-#'    \item{[CD3 capped cell: RCVCMP0000217](https://software.pixelgen.com/datasets/cd3-capping-v1.0-immunology-I)}
+#'    \item{[CD3 capping stimulated CD4 T cell: RCVCMP0000217](https://software.pixelgen.com/datasets/cd3-capping-v1.0-immunology-I)}
 #'    \item{[B cell: RCVCMP0000118, CD4 T cell: RCVCMP0000487, CD8 T cell: RCVCMP0000655](https://software.pixelgen.com/datasets/1k-human-pbmcs-v1.0-immunology-I))}
-#'    \item{[Uropod cell: RCVCMP0000263](https://software.pixelgen.com/datasets/uropod-t-cells-v1.0-immunology-I)}
+#'    \item{[Migratory CD8 T cell with uropod: RCVCMP0000263](https://software.pixelgen.com/datasets/uropod-t-cells-v1.0-immunology-I)}
 #' }
 #'
 #' @name pxl_dataset
