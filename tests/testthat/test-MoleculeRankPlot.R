739dac52e6ff5f2074f91541b2476f202daf52dd
ludvigla <ludvig.larsson@pixelgen.com>
2024-06-24 15:07:09 +0200

    Cleaned up examples/tests

--- tests/testthat/test-MoleculeRankPlot.R
+++ tests/testthat/test-MoleculeRankPlot.R
@@ -5,13 +5,13 @@ pxl_file <- system.file("extdata/five_cells",
 seur_obj <- ReadMPX_Seurat(pxl_file, overwrite = TRUE)
 
 test_that("MoleculeRankPlot works for Seurat objects", {
-  expect_no_error({cellrank_plot <- MoleculeRankPlot(seur_obj)})
-  expect_s3_class(cellrank_plot, "ggplot")
-  expect_no_error({cellrank_plot <- MoleculeRankPlot(seur_obj, group_by = "leiden")})
+  expect_no_error({moleculerank_plot <- MoleculeRankPlot(seur_obj)})
+  expect_s3_class(moleculerank_plot, "ggplot")
+  expect_no_error({moleculerank_plot <- MoleculeRankPlot(seur_obj, group_by = "leiden")})
 })
 
 test_that("MoleculeRankPlot works for data.frame-like objects", {
-  expect_no_error({cellrank_plot <- MoleculeRankPlot(seur_obj[[]])})
-  expect_s3_class(cellrank_plot, "ggplot")
+  expect_no_error({moleculerank_plot <- MoleculeRankPlot(seur_obj[[]])})
+  expect_s3_class(moleculerank_plot, "ggplot")
 })
 
