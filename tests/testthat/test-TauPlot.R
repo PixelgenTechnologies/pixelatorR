de5068c6abbd4d3023c827c3f0bc230db664bb79
ludvigla <ludvig.larsson@pixelgen.com>
2024-07-10 10:27:50 +0200

    updated TauPlot for compatibility with 0.18 metric names

--- tests/testthat/test-TauPlot.R
+++ tests/testthat/test-TauPlot.R
@@ -6,6 +6,23 @@ seur_obj <- ReadMPX_Seurat(pxl_file)
 test_that("TauPlot works for Seurat objects", {
   expect_no_error({tau_plot <- TauPlot(seur_obj)})
   expect_s3_class(tau_plot, "ggplot")
+  expect_equal(
+    structure(list(x = ~tau, y = ~umi_per_upia, colour = ~tau_type), class = "uneval"),
+    tau_plot$mapping
+  )
+
+  # With 0.18 PXL file format
+  expect_no_error(
+    seur_obj@meta.data <- seur_obj[[]] %>%
+      rename(mean_molecules_per_a_pixel = mean_umi_per_upia) %>%
+      select(-all_of("umi_per_upia"))
+  )
+  expect_no_error({tau_plot <- TauPlot(seur_obj)})
+  expect_s3_class(tau_plot, "ggplot")
+  expect_equal(
+    structure(list(x = ~tau, y = ~mean_molecules_per_a_pixel, colour = ~tau_type), class = "uneval"),
+    tau_plot$mapping
+  )
 })
 
 test_that("TauPlot works for data.frame-like objects", {
