153701223f8959244e84e33f68f9ed90c02a6b03
maxkarlsson <40792056+maxkarlsson@users.noreply.github.com>
2024-10-08 10:51:30 +0200

    feature: better colocalization heatmap legend

--- R/colocalization_heatmap.R
+++ R/colocalization_heatmap.R
@@ -12,6 +12,9 @@
 #' instead of drawing the heatmap
 #' @param symmetrise Set to \code{TRUE} if only the lower or upper triangle of marker combinations exist in \code{data},
 #'                   each row will then be mirrored to fill the missing triangle of the heatmap.
+#' @param legend_range A numeric vector of length 2 with the range of the legend. If NULL, the range is set to the
+#'                     maximum absolute value of the data
+#' @param legend_title The title of the legend
 #' @param ... Parameters passed to \code{pheatmap}
 #'
 #' @return A heatmap
@@ -30,24 +33,35 @@ ColocalizationHeatmap <- function(
   ),
   return_plot_data = FALSE,
   symmetrise = TRUE,
+  legend_range = NULL,
+  legend_title = "",
   ...
 ) {
   # Check if pheatmap is installed
-  expect_pheatmap()
+  expect_ComplexHeatmap()
 
   # Validate data
   if (!inherits(data, what = "tbl_df")) {
     abort(glue("'data' must be a 'tbl_df' object"))
   }
   stopifnot(
-    "'marker1_col' is not in 'data'" = marker1_col %in% names(data),
-    "'marker2_col' is not in 'data'" = marker2_col %in% names(data),
-    "'value_col' is not in 'data'" = value_col %in% names(data),
+    "'marker1_col' is not in 'data'" =
+      marker1_col %in% names(data),
+    "'marker2_col' is not in 'data'" =
+      marker2_col %in% names(data),
+    "'value_col' is not in 'data'" =
+      value_col %in% names(data),
     "'colors' must be a character vector with at least 2 colors" =
       inherits(colors, what = "character") &&
         (length(colors) > 1),
-    "'return_plot_data' must be a logical" = inherits(return_plot_data, what = "logical"),
-    "'symmetrise' must be a logical" = inherits(symmetrise, what = "logical")
+    "'return_plot_data' must be a logical" =
+      inherits(return_plot_data, what = "logical"),
+    "'symmetrise' must be a logical" =
+      inherits(symmetrise, what = "logical"),
+    "'legend_range' must be a numeric vector of length 2" =
+      is.null(legend_range) || (inherits(legend_range, what = "numeric") && length(legend_range) == 2),
+    "'legend_title' must be a character" =
+      inherits(legend_title, what = "character") & length(legend_title) == 1
   )
 
 
@@ -84,13 +98,20 @@ ColocalizationHeatmap <- function(
   }
 
   # Set range for heatmap legend
-  legend_range <- max(abs(plot_data))
+  if (is.null(legend_range)) {
+    legend_range <- max(abs(plot_data)) * c(-1, 1)
+  }
+
+  plot_data[plot_data < legend_range[1]] <- legend_range[1]
+  plot_data[plot_data > legend_range[2]] <- legend_range[2]
 
   # Plot heatmap
   plot_data %>%
-    pheatmap::pheatmap(
-      breaks = seq(-legend_range, legend_range, length.out = 101),
+    as.matrix() %>%
+    ComplexHeatmap::pheatmap(
+      breaks = seq(legend_range[1], legend_range[2], length.out = 101),
       color = colorRampPalette(colors)(100),
+      heatmap_legend_param = list(title = legend_title),
       ...
     )
 }
