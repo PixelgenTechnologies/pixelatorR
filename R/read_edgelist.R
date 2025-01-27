e0dcac187189e360d1a3597a01676ed2752474cd
ludvigla <ludvig.larsson@pixelgen.com>
2025-01-27 09:58:11 +0100

    Minor bug fixes

--- R/read_edgelist.R
+++ R/read_edgelist.R
@@ -49,8 +49,11 @@ ReadMPX_arrow_edgelist <- function(
   if (!is.null(edge_list_file)) {
     assert_single_value(edge_list_file, type = "string")
     assert_file_ext(edge_list_file, ext = "parquet")
+    edge_list_dir <- dirname(edge_list_file)
+    fname <- basename(edge_list_file)
   } else {
     edge_list_dir <- fs::path_temp()
+    fname <- "edgelist.parquet"
     if (verbose && check_global_verbosity()) {
       cli_alert_info("Extracting edgelist.parquet file to {col_br_blue(file.path(edge_list_dir, 'edgelist.parquet'))}")
     }
@@ -58,9 +61,12 @@ ReadMPX_arrow_edgelist <- function(
 
   # Extract the edgelist.parquet file
   utils::unzip(pxl_file, files = "edgelist.parquet", exdir = edge_list_dir)
+  if (!is.null(edge_list_file)) {
+    fs::file_move(fs::path(edge_list_dir, "edgelist.parquet"), fs::path(edge_list_dir, fname))
+  }
 
   # Read the parquet file
-  ds <- arrow::open_dataset(fs::path(edge_list_dir, "edgelist.parquet"))
+  ds <- arrow::open_dataset(fs::path(edge_list_dir, fname))
 
   # Convert selected columns uint64 to string
   sel_fields <- grep(pattern = "^umi", names(ds), value = TRUE)
--- tests/testthat/test-ReadMPX_edgelist.R
+++ tests/testthat/test-ReadMPX_edgelist.R
@@ -1,5 +1,7 @@
+pxl_file <- system.file("extdata/five_cells", "five_cells.pxl", package = "pixelatorR")
+
 test_that("ReadMPX_arrow_edgelist works as expected", {
-  el <- ReadMPX_arrow_edgelist(system.file("extdata/five_cells", "five_cells.pxl", package = "pixelatorR"))
+  el <- ReadMPX_arrow_edgelist(pxl_file)
   expect_type(el, type = "environment")
   expect_equal(names(el), c(
     "upia", "upib", "marker", "count",
@@ -7,9 +9,20 @@ test_that("ReadMPX_arrow_edgelist works as expected", {
   ))
   expect_no_error({
     msg <- capture_messages({
-      el <- ReadMPX_arrow_edgelist(system.file("extdata/five_cells", "five_cells.pxl", package = "pixelatorR"))
+      el <- ReadMPX_arrow_edgelist(pxl_file)
     })
   })
+
+  tmp_f <- fs::path_temp("test.parquet")
+  expect_no_error({
+    el <-
+      ReadMPX_arrow_edgelist(
+        pxl_file,
+        edge_list_file = tmp_f,
+        verbose = FALSE
+      )
+  })
+  expect_true(fs::file_exists(tmp_f))
 })
 
 
@@ -17,6 +30,10 @@ test_that("ReadMPX_arrow_edgelist fails when invalid input is provided", {
   expect_error({
     el <- ReadMPX_arrow_edgelist("Invalid file",
       outdir = tempdir()
+    )
+  })
+  expect_error({
+    el <- ReadMPX_arrow_edgelist("Invalid file", edge_list_file = fs::path_temp("test.pxl")
     )
   })
 })
