3abd2c2f108d1130bc7aace5720ce8d566f52ecb
maxkarlsson <max.karlsson@pixelgen.com>
2026-01-19 17:35:18 +0100

    feat: reference pbmc data

--- R/annotate.R
+++ R/annotate.R
@@ -545,3 +545,47 @@ AnnotateCells <- function(
 
   return(W)
 }
+
+
+#' Read PBMC cell annotation reference
+#'
+#' This function reads the cell annotation reference data from the pixelatorR package's extdata directory. This data
+#' contains 720 annotated PBMC components with their corresponding counts of 158 proteins. The annotation has been
+#' performed at two levels of granularity: "celltype.l1" (broad categories) and "celltype.l2" (more specific subtypes).
+#' When loading the data through this function, a Seurat object is created with the counts data stored in the "PNA"
+#' assay and the cell annotations included in the metadata.
+#'
+#' @return A Seurat object containing the cell annotation and counts data.
+#'
+#' @export
+#'
+read_pbmc_reference <-
+  function() {
+    cell_annotation <-
+      system.file(
+        "extdata", "PBMC_cell_annotation.csv",
+        package = "pixelatorR"
+      ) %>%
+      read.csv() %>%
+      column_to_rownames("sample_component")
+
+    counts <-
+      system.file(
+        "extdata", "PBMC_cell_counts.csv",
+        package = "pixelatorR"
+      ) %>%
+      read.csv(check.names = FALSE) %>%
+      column_to_rownames("sample_component")
+
+    counts <-
+      counts[rownames(cell_annotation), ] %>%
+      t() %>%
+      as("dgCMatrix")
+
+    # Create Seurat object
+    CreateSeuratObject(
+      counts = counts,
+      assay = "PNA",
+      meta.data = cell_annotation
+    )
+  }
