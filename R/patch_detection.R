6f374015fdd880b0cec60757cd67663371614175
ludvigla <ludvig.larsson@pixelgen.com>
2026-04-30 07:27:10 +0200

    Reverted to use ngMatrix

--- R/patch_detection.R
+++ R/patch_detection.R
@@ -824,7 +824,7 @@ expand_adjacency_matrix <- function(
       A <- (A / Matrix::rowSums(A)) %>% Matrix::t()
     } else {
       # Switch to pattern matrix for faster multiplication when not using weights
-      A <- as(A, "nCMatrix")
+      A <- as(A, "ngCMatrix")
     }
     # Expand neighborhood
     if (min_weight > 0 && use_weights) {
