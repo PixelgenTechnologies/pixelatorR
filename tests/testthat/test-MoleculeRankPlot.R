59275f7d8cee789e7e388c23353faa6aee3bc114
ludvigla <ludvig.larsson@pixelgen.com>
2026-06-22 19:11:54 +0200

    Removed tau_type from test

--- tests/testthat/test-MoleculeRankPlot.R
+++ tests/testthat/test-MoleculeRankPlot.R
@@ -3,6 +3,7 @@ pxl_file_mpx <- minimal_mpx_pxl_file()
 seur_obj_mpx <- ReadMPX_Seurat(pxl_file_mpx)
 pxl_file_pna <- minimal_pna_pxl_file()
 seur_obj_pna <- ReadPNA_Seurat(pxl_file_pna)
+seur_obj_pna$sample_id <- c("S1", "S1", "S2", "S2", "S3")
 
 test_that("MoleculeRankPlot works for Seurat objects", {
   expect_no_error({
@@ -17,7 +18,7 @@ test_that("MoleculeRankPlot works for Seurat objects", {
   })
   expect_s3_class(moleculerank_plot, "ggplot")
   expect_no_error({
-    moleculerank_plot <- MoleculeRankPlot(seur_obj_pna, group_by = "tau_type")
+    moleculerank_plot <- MoleculeRankPlot(seur_obj_pna, group_by = "sample_id")
   })
 })
 
